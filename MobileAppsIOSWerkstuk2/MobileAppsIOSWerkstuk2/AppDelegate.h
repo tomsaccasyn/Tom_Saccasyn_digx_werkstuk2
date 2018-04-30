910f94933839907d8e259dbd01d685b01c177f05
Tom Saccasyn <student@students-mac.local>
2018-04-30 16:44:04 +0200

    proberen het project op de repo te zetten poging 2

---
+++ MobileAppsIOSWerkstuk2/MobileAppsIOSWerkstuk2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileAppsIOSWerkstuk2
+//
+//  Created by student on 30/04/2018.
+//  Copyright © 2018 EHB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileAppsIOSWerkstuk2/MobileAppsIOSWerkstuk2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MobileAppsIOSWerkstuk2
+//
+//  Created by student on 30/04/2018.
+//  Copyright © 2018 EHB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

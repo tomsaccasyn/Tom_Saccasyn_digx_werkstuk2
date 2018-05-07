67c4dd5ad073ebe42053fd50a15de8f2d19a194e
Tom Saccasyn <student@students-mac.local>
2018-05-07 09:53:43 +0200

    make the non swift project to a swift project

---
+++ MobileAppsIOSWerkstuk2/MobileAppsIOSWerkstuk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileAppsIOSWerkstuk2
+//
+//  Created by student on 07/05/2018.
+//  Copyright © 2018 EHB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
